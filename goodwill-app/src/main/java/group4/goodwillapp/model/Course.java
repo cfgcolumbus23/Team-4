51ae33c915320501eec8dbf7226aeb048895177b
Grayson L <63122082+Hebrooski@users.noreply.github.com>
2023-11-03 16:57:11 -0400

    added getters&setters

--- goodwill-app/src/main/java/group4/goodwillapp/model/Course.java
+++ goodwill-app/src/main/java/group4/goodwillapp/model/Course.java
@@ -15,5 +15,47 @@ public class Course{
     private boolean availabilty; //is the course available to take
     private String courseProvider;
     //for if goodwill over courses provided by other entities
+    public long getCourseId() {
+        return courseId;
+    }
+    public void setCourseId(long courseId) {
+        this.courseId = courseId;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public LocalDateTime getTimeApplied() {
+        return timeApplied;
+    }
+    public void setTimeApplied(LocalDateTime timeApplied) {
+        this.timeApplied = timeApplied;
+    }
+    public String getCourseLength() {
+        return courseLength;
+    }
+    public void setCourseLength(String courseLength) {
+        this.courseLength = courseLength;
+    }
+    public boolean isAvailabilty() {
+        return availabilty;
+    }
+    public void setAvailabilty(boolean availabilty) {
+        this.availabilty = availabilty;
+    }
+    public String getCourseProvider() {
+        return courseProvider;
+    }
+    public void setCourseProvider(String courseProvider) {
+        this.courseProvider = courseProvider;
+    }
 
 }
