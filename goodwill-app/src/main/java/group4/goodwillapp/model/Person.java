a106a2c983db9527a719a5059d69d4c6315c5731
rohan-nambiar <hellorohan.nambiar@gmail.com>
2023-11-03 18:15:00 -0400

    Fixed model merge issues

--- goodwill-app/src/main/java/group4/goodwillapp/model/Person.java
+++ goodwill-app/src/main/java/group4/goodwillapp/model/Person.java
@@ -5,6 +5,13 @@ import lombok.Setter;
 
 @Getter
 @Setter
+public class Person {
+    private String name;
+    private String email;
+    private String phoneNum;
+    private int age;
+    private String password;
+
   public Person(){}
 
   public Person(String name, String email, String phoneNum, int age, String password){
