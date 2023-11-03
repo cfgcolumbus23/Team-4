a22cad7fa049519b8f014718deffdc436e815479
rohan-nambiar <hellorohan.nambiar@gmail.com>
2023-11-03 17:13:47 -0400

    Setup Application Properties for Mongo

---
+++ goodwill-app/src/main/java/group4/goodwillapp/controller/MongodbController.java
@@ -0,0 +1,19 @@
+package group4.goodwillapp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MongodbController {
+
+    // private String mongoUri;
+
+
+    // @GetMapping("/test")
+    // public String mongoDBUri() {
+    //     return mongoUri;
+    // }
+    
+}
---
+++ goodwill-app/src/main/java/group4/goodwillapp/service/MongoDBService.java
@@ -0,0 +1,16 @@
+package group4.goodwillapp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+// import group4.goodwillapp.repository.MongoDBRepository;
+
+public class MongoDBService {
+
+    // @Autowired
+    // private MongoDBRepository mongoDBRepository;
+
+    // public void savePerson(){
+    //     mongoDBRepository.save(null);
+    // }
+    
+}
