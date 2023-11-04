1eb82e0a7bef7b31d4e03a7115d73f742528a628
Grayson L <63122082+Hebrooski@users.noreply.github.com>
2023-11-04 09:56:32 -0400

    removed unused imports

--- goodwill-app/src/main/java/group4/goodwillapp/config/WebSocketConfig.java
+++ goodwill-app/src/main/java/group4/goodwillapp/config/WebSocketConfig.java
@@ -1,7 +1,6 @@
 package group4.goodwillapp.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
