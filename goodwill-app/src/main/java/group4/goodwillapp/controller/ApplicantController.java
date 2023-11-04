c8c3b0d1f6a0889497d3e95acc66929b378a2600
Grayson L <63122082+Hebrooski@users.noreply.github.com>
2023-11-03 20:55:37 -0400

    updated the repo and controller

--- goodwill-app/src/main/java/group4/goodwillapp/controller/ApplicantController.java
+++ goodwill-app/src/main/java/group4/goodwillapp/controller/ApplicantController.java
@@ -1,5 +1,8 @@
 package group4.goodwillapp.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,17 +30,19 @@ public class ApplicantController {
     }
 
     @GetMapping("/getApplicant/{appId}")
-    public ResponseEntity<Course> getApplicantById(@PathVariable Long applicantId) {
+    public ResponseEntity<Applicant> getApplicantById(@PathVariable Long applicantId) {
         return mongoDBService.getApplicantById(applicantId)
                 .map(applicant -> new ResponseEntity<>(applicant, HttpStatus.OK))
                 .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @GetMapping("/getAllApplicants")
-    public ResponseEntity<Course> getAllCourses(@PathVariable Long courseId) {
-        return mongoDBService.getCourseById(courseId)
-                .map(course -> new ResponseEntity<>(course, HttpStatus.OK))
-                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    public ResponseEntity<List<Applicant>> getAllApplicants() {
+        try{
+            return new ResponseEntity<List<Applicant>>(mongoDBService.getAllApplicants(), HttpStatus.OK);
+        }catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }  
     }
 
     
