f55da9f5a7d7cb8779c7fc099fa02a7c19f0abb1
rohan-nambiar <hellorohan.nambiar@gmail.com>
2023-11-04 01:11:27 -0400

    Checked Angular Functionality

--- goodwill-app/src/main/java/group4/goodwillapp/controller/FeedbackController.java
+++ goodwill-app/src/main/java/group4/goodwillapp/controller/FeedbackController.java
@@ -9,6 +9,7 @@ import group4.goodwillapp.service.FeedbackService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/feedback")
 public class FeedbackController {
 
