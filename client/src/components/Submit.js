bdcb4ff16a165db82a336b1a6cf88dd1b58a8232
lifrank1 <frank.li.865985@gmail.com>
2023-11-03 20:17:49 -0400

    creates submit button and added comments

---
+++ client/src/components/Submit.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Checkbox from './Checkbox';
+
+
+//create a select all that apply box
+const Submit = () => {
+  return (
+    <div class="select-all-apply-menu align-left">
+      <button type="Submit">Submit</button>
+    </div>
+  );
+};
+
+export default Submit;
