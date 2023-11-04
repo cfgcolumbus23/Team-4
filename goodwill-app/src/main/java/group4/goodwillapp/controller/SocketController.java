f0f41e3f140252e972ba30d6c880d02097a02595
lifrank1 <frank.li.865985@gmail.com>
2023-11-04 07:00:04 -0400

    created working chat 2 way

--- goodwill-app/src/main/java/group4/goodwillapp/controller/SocketController.java
+++ goodwill-app/src/main/java/group4/goodwillapp/controller/SocketController.java
@@ -4,9 +4,11 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import group4.goodwillapp.model.MessageBean;
 
+//@CrossOrigin(origins = "http://localhost:3000")
 @Controller
 public class SocketController {
 
